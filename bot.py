e2c205424508fd63d56d8438302a806421d2fd3f
S Stewart <iamtheworst@programmer.net>
2018-04-14 10:05:17 -0500

    some changes to core

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from ruamel.yaml import YAML
 import logging
+import sys
 
 rlog = logging.getLogger()
 rlog.setLevel(logging.INFO)
@@ -15,14 +16,20 @@ yaml = YAML(typ='safe')
 conf_file = open('config.yaml').read().rstrip()
 config = yaml.load(conf_file)
 
+
 class HahaYes(commands.AutoShardedBot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         yaml = YAML(typ='safe')
         self.config = yaml.load(open('config.yaml').read().rstrip())
 
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            e = error.param
+            return await ctx.send(f':x: Missing required argument: `{e}`')
+
     async def on_ready(self):
-      rlog.info("""
+        rlog.info("""
                                   .'  .
                          .'.'.' .
                         .`.'.`'.
@@ -44,7 +51,8 @@ class HahaYes(commands.AutoShardedBot):
       _.-dX8X8Xbd8X8X8X8X8X8XdbX8X8Xb-._
    .-d8X8X8X8bdX8X8X8X8X8X8X8X8db8X8X8X8b-.
 .-d8X8X8X8X8bdX8X8X8X8X8X8X8X8X8db8X8X8-RG-b-.""")
-      rlog.info("Hi. I'm Lava.")
+        rlog.info("Hi. I'm Lava.")
+
 
 bot = HahaYes(command_prefix=commands.when_mentioned_or(config['prefix']),
               description='A simple music bot.',
@@ -58,7 +66,7 @@ if __name__ == '__main__':
         bot.load_extension('cogs.load')
     except Exception:
         rlog.error('OOPSIE WOOPSIE!! Uwu We made a fucky wucky!!')
-        rlog.error(' A wittle fucko boingo! The code monkeys at our') 
+        rlog.error(' A wittle fucko boingo! The code monkeys at our')
         rlog.error(' headquarters are working VEWY HAWD to fix this!')
         rlog.error(' ', exc_info=True)
     else:
